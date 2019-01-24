98f4c0a8109703a8aa7e06bf85db06bc003b3825
seanmcquaid <sean.mcquaid2@verizon.net>
2019-01-24 11:57:37 -0500

    connected database data and res.json the data

--- routes/index.js
+++ routes/index.js
@@ -7,13 +7,20 @@ const config = require("../config");
 var connection = mysql.createConnection(config);
 
 connection.connect();
-
+console.log("I'm connected");
 router.get('/', function(req, res, next) {
   // we want to load up a list of our restaurants on the home page
   // these are inside of my sql
   // inside this route, BEFORE we res.render a view
   // we want to query the database, get the data then we can send it over to the view
-
+  const query = "SELECT * FROM restaurant;"
+  connection.query(query, (err,results)=>{
+    if(err){
+      throw err;
+    } else {
+      res.json(results);
+    }
+  });
 });
 
 module.exports = router;
